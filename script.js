d0a855f04bb83b3aa4925b6c80202e7adfc49e25
Chem-e <38943210+Chem-e@users.noreply.github.com>
2018-06-12 16:33:31 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function cost() {
 	let costOfApples= 10;
 	let costOfMangoes= 8;
 	let costOfBananas= 15;
-	let noOfApple=document.querySelector("#Apples").value; 
-	let noOfMangoes=document.querySelector("#Mangoes").value;
-	let noOfBananas=document.querySelector("#Bananas").value;
+	let noOfApple=parseInt(document.querySelector("#Apples").value); 
+	let noOfMangoes=parseInt(document.querySelector("#Mangoes").value);
+	let noOfBananas=parseInt(document.querySelector("#Bananas").value);
 	let totalCost= costOfApples*noOfApple+costOfMangoes*noOfMangoes+costOfBananas*noOfBananas;
 	alert(totalCost);
 
